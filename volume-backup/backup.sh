e95c4d4586545f0e1400d179f31378da2b9e7719
Matthew Smith <mjsmith92@outlook.com>
2026-04-03 14:37:34 -0400

    Take proper db backups

--- volume-backup/backup.sh
+++ volume-backup/backup.sh
@@ -9,14 +9,20 @@ ARCHIVE_NAME="backup-$DATE.zip"
 
 mkdir -p $TMP_DIR
 
+echo "Backing up SQLite databases..."
+sqlite3 /data/pihole_data/gravity.db ".backup '$TMP_DIR/gravity.db'"
+sqlite3 /data/pihole_data/pihole-FTL.db ".backup '$TMP_DIR/pihole-FTL.db'"
+sqlite3 /data/budget_data/budget.db ".backup '$TMP_DIR/budget.db'"
+
 echo "Creating archive..."
 zip -r "$TMP_DIR/$ARCHIVE_NAME" "$SOURCE_DIR"
+zip "$TMP_DIR/$ARCHIVE_NAME" "$TMP_DIR/gravity.db" "$TMP_DIR/pihole-FTL.db" "$TMP_DIR/budget.db"
 
 echo "Uploading to OneDrive..."
 rclone copy "$TMP_DIR/$ARCHIVE_NAME" onedrive:/Documents/Homelab/Backups
 
-echo "Removing local temp file..."
-rm "$TMP_DIR/$ARCHIVE_NAME"
+echo "Removing local temp files..."
+rm "$TMP_DIR/$ARCHIVE_NAME" "$TMP_DIR/gravity.db" "$TMP_DIR/pihole-FTL.db" "$TMP_DIR/budget.db"
 
 echo "Deleting remote files older than 7 days..."
 rclone delete onedrive:/Documents/Homelab/Backups --min-age 7d
